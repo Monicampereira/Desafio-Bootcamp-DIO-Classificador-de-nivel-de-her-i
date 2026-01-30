048c05cd1480952d865eb4366a1900735abb7d5f
Monicampereira <92582542+Monicampereira@users.noreply.github.com>
2026-01-30 20:24:44 -0300

    Add files via upload

---
+++ Bootcamp dio.js
@@ -0,0 +1,23 @@
+let nome = "Vitor";
+let xp = 3000;
+let nivel = "";
+
+if  (xp < 1000 ) {
+  nivel = "ferro"
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "bronze"
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata"
+ } else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro"
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "bronze"
+} else if (xp > 8001 && xp <= 9000) {
+  nivel = "Ascendente"
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal"
+} else if (xp >= 10.001) {
+  nivel = "Radiante"
+}
+
+console.log(" O herói "   +  nome  +  " está no nível: " + nivel )
